8d3b0666b450d0aa3571bfe9f643ca3bcdb77ee4
tianxiang321 <1129137164@qq.com>
2018-05-04 20:39:26 +0800

    完成简易版MVVM

--- javaScript设计模式/第三十九章-MVVM.js
+++ javaScript设计模式/第三十九章-MVVM.js
@@ -7,141 +7,17 @@
 
 //通过属性劫持 完成 MVVM
 var html=`
-    <input type="text" v-model="msg">{{msg}}<p>{{msg2}}</p>
+    <input type="text" v-model="msg">{{msg}}<p>{{msg2}}</p><p>{{msg}}</p>
 `;
 var div = document.createElement('div');
 div.id='app';
 div.innerHTML = html;
 document.body.appendChild(div);
 
-function Mvvm(opt){
-    this.el=opt.el;
-    this.data=opt.data;
-    observer(this.data,this);
-    var el = document.querySelector(this.el);
-    var dom = nodeToFragment(el,this);
-    el.appendChild(dom);
-}
-
-//nodeToFragment
-function nodeToFragment(node,mv){
-    var frag = document.createDocumentFragment(),child;
-    while(child = node.firstChild){
-        compile(child,mv);
-        frag.appendChild(child);
-    }
-    return frag;
-}
-
-//compile
-function compile(node,mv){
-    if(node.nodeType === 1){
-        if(node.childNodes){
-            var nodes =[...node.childNodes];
-            nodes.forEach(function(node){
-                compile(node,mv);
-            })
-        }
-        var attrs = [...node.attributes];
-        attrs.forEach(function(attr){
-            if(attr.nodeName === 'v-model'){
-                var name = attr.nodeValue;
-                node.addEventListener('input',function(e){
-                    mv[name] = e.target.value;
-                });
-                node.value = mv[name];
-                node.removeAttribute('v-model');
-                new Watch(mv,node,name,'input');
-            }
-        })
-    }
-    if(node.nodeType === 3){
-        if(/\{\{(.*)\}\}/.test(node.nodeValue)){
-            var name = RegExp.$1;
-            name=name.trim();
-            new Watch(mv,node,name,'text');
-        }
-    }
-}
-
-//Watch
-function Watch(mv,node,name,nodeType){
-    Dep.target=this;
-    this.mv=mv;
-    this.node=node;
-    this.name=name;
-    this.nodeType=nodeType;
-    this.update();
-    Dep.target=null;
-}
-Watch.prototype={
-    update:function(){
-        this.get();
-        if(this.nodeType === 'input'){
-            this.node.value=this.value;
-        }
-        if(this.nodeType === 'text'){
-            this.node.nodeValue=this.value;
-        }
-    },
-    get:function(){
-        this.value=this.mv[this.name];
-    }
-};
-
-//observer
-function observer(data,mv){
-    Object.keys(data).forEach(function(key){
-        defineReactive(mv,key,data[key]);
-    })
-}
-
-//defineReactive
-function defineReactive(mv,key,val){
-    var dep = new Dep();
-    Object.defineProperty(mv,key,{
-        get:function(){
-            if(Dep.target){
-                dep.addSub(Dep.target)
-            }
-            return val;
-        },
-        set:function(newValue){
-            if(newValue === val) return;
-            val = newValue;
-            dep.notify();
-        }
-    })
-}
-
-//Dep
-function Dep(){
-    this.subs=[];
-}
-Dep.prototype={
-    addSub:function(sub){
-        this.subs.push(sub);
-    },
-    notify:function(){
-        this.subs.forEach(function(sub){
-            sub.update();
-        })
-    }
-};
-
-/*new Mvvm({
-    el:'#app',
-    data:{
-        msg:'hello world!',
-        msg2:'this is msg2!'
-    }
-});*/
-
-
 var Model = {
-    msg:'hello world'
+    msg:'hello world',
+    msg2:'hello world2'
 };
-
 var View = {
     init:function(el){
         //将数据与View绑定
@@ -149,6 +25,7 @@ var View = {
         //解析Dom
         this.processNode(el);
     },
+    subs:[],
     processNode:function(el){
         var node = document.querySelector(el);
         var frag = document.createDocumentFragment(),child;
@@ -159,27 +36,72 @@ var View = {
         node.appendChild(frag);
     },
     compile:function(node){
-        console.log(View.msg);
-        View.msg = 'oooo';
+        function Sub(node,name,nodeType){
+            this.node = node;
+            this.name = name;
+            this.nodeType = nodeType;
+        }
+        var self = this;
+        if(node.nodeType === 1){
+            if(node.childNodes){
+                var nodes =[...node.childNodes];
+                nodes.forEach(function(node){
+                    self.compile(node);
+                })
+            }
+            var attrs = [...node.attributes];
+            attrs.forEach(function(attr){
+                if(attr.nodeName === 'v-model'){
+                    var name = attr.nodeValue;
+                    node.addEventListener('input',function(e){
+                        self[name] = e.target.value;
+                    });
+                    node.value = self[name];
+                    node.removeAttribute('v-model');
+                    var sub = new Sub(node,name,'input');
+                    self.render(sub);
+                    self.subs.push(sub);
+                }
+            })
+        }
+        if(node.nodeType === 3){
+            if(/\{\{(.*)\}\}/.test(node.nodeValue)){
+                var name = RegExp.$1;
+                name=name.trim();
+                var sub = new Sub(node,name,'text');
+                self.render(sub);
+                self.subs.push(sub);
+            }
+        }
+    },
+    update:function(){
+        var self = this;
+        this.subs.forEach(function(sub){
+            self.render(sub);
+        })
+    },
+    render:function(sub){
+        if(sub.nodeType === 'input'){
+            sub.node.value=this[sub.name];
+        }
+        if(sub.nodeType === 'text'){
+            sub.node.nodeValue=this[sub.name];
+        }
     }
 };
-
 var ViewModel={
     bind:function(m){
         Object.keys(m).forEach(function(key){
             Object.defineProperty(View,key,{
                 get:function(){
-                    console.log('get');
                     return m[key];
                 },
                 set:function(newVal){
-                    console.log('set');
                     m[key] = newVal;
+                    this.update();
                 }
             })
         });
-
     }
 };
-
-console.log(View.init('#app'));
+View.init('#app');
