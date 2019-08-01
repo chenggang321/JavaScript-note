048c1352a890668b68f2cce71e134cbbe3608532
chengganghy <chenggang@ruyucapital.com>
2019-08-01 15:41:11 +0800

    单例模式修改

--- 03-javaScript设计模式/第八章-一个人的寂寞-单例模式.js
+++ 03-javaScript设计模式/第八章-一个人的寂寞-单例模式.js
@@ -2,8 +2,10 @@
  * Created by HH_Girl on 2018/2/5.
  */
 /*
- * 单例模式：又被称为单体模式，是只允许实例化一次的对象类。有时我们也
- * 用一个对象来规划一个命名空间，仅仅有条的管理对象上的属性与方法
+ * 单例模式：又被称为单体模式，是只允许实例化一次的对象类。保证一个类仅有一个实例，
+ * 并提供访问此实例的全局访问点。
+ *
+ * 作用：保证对象不被重复创建，以达到降低开销
  * */
 //创建一个小小型代码库
 var cg = {
@@ -55,7 +57,7 @@ console.log(count);
 //惰性载入单例
 var LazySingle=(function(){
     //单例实例引用
-    var _intance=null;
+    var _instance=null;
     function Single(){
         //这里定义私有属性和方法
         return {
@@ -66,11 +68,25 @@ var LazySingle=(function(){
     //获取单例对象接口
     return function(){
         //如果为创建单例将创建单例
-        if(!_intance){
-            _intance=Single();
+        if(!_instance){
+            _instance= Single();
         }
         //返回单例
-        return _intance;
+        return _instance;
     }
 })();
 console.log(LazySingle().publicProperty);
+
+const Singleton = function(){};
+Singleton.getInstance = (function(){
+    let instance = null;
+    return function(){
+        if(!instance){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+})();
+let s1 = Singleton.getInstance();
+let s2 = Singleton.getInstance();
+console.log(s1 === s2);
