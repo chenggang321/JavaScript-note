84a6451fc23c98db9693775cb02acddc984a52fe
chengganghy <chenggang@ruyucapital.com>
2019-08-02 13:54:14 +0800

    享元模式实例代码添加

--- 03-javaScript设计模式/第十五章-城市公交车-享元模式.js
+++ 03-javaScript设计模式/第十五章-城市公交车-享元模式.js
@@ -4,6 +4,15 @@
 /*
  * 享元模式：运用共享技术有效地支持大量的细粒度的对象，
  * 避免对象间拥有相同内容造成多余的开销
+ *
+ * 享元模式：运用共享技术来减少创建对象的数量，从而减少内存占用、提高性能。
+ * 1.享元模式提醒我们将对象的属性划分为 内部和外部状态。
+ *      内部状态：可以被对象集合共享，通常不会改变
+ *      外部状态：根据应用场景经常改变
+ * 2.享元模式是利用时间换空间的优化模式。
+ *
+ * 应用场景：只要是需要大量创建重复的类的代码块，均可以用享元模式抽离内部/外
+ * 部状态，减少重复类的创建。
  * */
 var article = [{
     id: 1,
@@ -113,32 +122,92 @@ var Flyweight = function () {
         }
     }
 }();
-    var page=0,
-        num=5,
-        len=article.length;
-    for(var i=0;i<5;i++){
-        if(article[i].title){
-            Flyweight.getDiv().innerHTML=article[i].title;
-        }
+var page = 0,
+    num = 5,
+    len = article.length;
+for (var i = 0; i < 5; i++) {
+    if (article[i].title) {
+        Flyweight.getDiv().innerHTML = article[i].title;
     }
+}
 //下一页
 var btn = document.createElement('button');
 btn.innerHTML = '下一页';
 btn.onclick = function () {
-    if(len<5){
+    if (len < 5) {
         return;
     }
     var n = ++page % Math.ceil(len / num) * num;
-    for (var j=0; j < 5; j++) {
-       if(article[n+j]){
-           Flyweight.getDiv().innerHTML=article[n+j].title;
-       }else if(article[n+j-len]){
-           console.log(n+j-len);
-           Flyweight.getDiv().innerHTML=article[n+j-len].title;
-       }else{
-           Flyweight.getDiv().innerHTML='';
-       }
+    for (var j = 0; j < 5; j++) {
+        if (article[n + j]) {
+            Flyweight.getDiv().innerHTML = article[n + j].title;
+        } else if (article[n + j - len]) {
+            console.log(n + j - len);
+            Flyweight.getDiv().innerHTML = article[n + j - len].title;
+        } else {
+            Flyweight.getDiv().innerHTML = '';
+        }
     }
 };
 document.body.appendChild(btn);
 
+// 通用代码池类，管理一个装载空闲对象的数组，如果外部需要一个对象，直接从对象
+// 中获取，而不是通过new创建
+class ObjectPool {
+    constructor() {
+        // 对象池
+        this._pool = [];
+    }
+
+    // 创建对象
+    create(Obj) {
+        return this._pool.length === 0
+            ? new Obj(this) // 对象池中没有空闲对象，则创建一个新的对象
+            : this._pool.shift(); // 对象池中有空闲对象，直接取出，无需再次创建
+    }
+
+    // 对象回收
+    recover(obj){
+        return this._pool.push(obj);
+    }
+
+    // 对象池大小
+    size(){
+        return this._pool.length
+    }
+}
+
+// 文件对象
+class File{
+    constructor(pool){
+        this.pool = pool
+    }
+
+    // 下载
+    download(){
+        console.log(`+ ${this.src} 开始下载 ${this.name}`);
+        setTimeout(()=>{
+            console.log(`- ${this.name} 下载完毕`);
+            this.pool.recover(this); // 下载完毕重新放入对象池
+        },1)
+    }
+}
+
+let objPool = new ObjectPool();
+
+Array.from({length:10}).forEach((item,index)=>{
+    setTimeout(()=>{
+        let file = objPool.create(File);
+        file.name = "文件"+index;
+        file.src = "http://download"+index+".com";
+        file.download();
+    },index*3)
+})
+
+setTimeout(
+    () =>
+        console.log(
+            `${"*".repeat(50)}\n创建了${objPool.size()}个对象`
+        ),
+    1000
+);
